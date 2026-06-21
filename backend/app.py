d23b34f29d891c059c0b92cd14d24eca10385790
cephasgm <cephasmkama@gmail.com>
2026-06-22 01:02:01 +0300

    Update backend with Arduino detection improvements

--- backend/app.py
+++ backend/app.py
@@ -43,8 +43,6 @@ if not firebase_admin._apps:
 
     firebase_admin.initialize_app(cred)
 
-firebase_admin.initialize_app(cred)
-
 db = firestore.client()
 
 print("FIREBASE PROJECT:", firebase_admin.get_app().project_id)
